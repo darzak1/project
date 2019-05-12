1436453065d92e9a3cffad54ecd69489a4d9d191
darzak1 <darzak@gmail.com>
2019-05-12 12:17:14 +0200

    konfiguracja bazy danych

---
+++ src/main/java/pl/edu/wszib/project/controller/HelloController.java
@@ -0,0 +1,20 @@
+package pl.edu.wszib.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //przekazuje dane za pomocą http
+
+public class HelloController {
+
+    @GetMapping("/hello")// po slashu jest koncówka adresu strny gdzie to ma być wyświetlone np tu: http://localhost:8080/hello
+    public String helloPage() {
+        return "Hello!!!";
+    }
+
+    @GetMapping ("/powitanie")
+    public String stronaPowitalna(){
+        return "To jest strona powitalna  " + HelloController.class.getCanonicalName();
+    }
+
+}
